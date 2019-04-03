c7eb0c470c2803b17e887855447f246a8285aec2
Youri Roggeveen <y.roggeveen@gmail.com>
2019-04-03 15:30:17 +0200

    cleanup and prettier

--- src/components/app-item.js
+++ src/components/app-item.js
@@ -1,40 +1,72 @@
-import React from 'react'
+import React from 'react';
 
 export default class AppItem extends React.Component {
+	render() {
+		const { checked, onToggle, name, title, description, t } = this.props;
+		const required = this.props.required || false;
+		const optOut = this.props.optOut || false;
+		const purposes = this.props.purposes || [];
+		const onChange = e => {
+			onToggle(e.target.checked);
+		};
+		const id = `app-item-${name}`;
+		const purposesText = purposes
+			.map(purpose => t(['purposes', purpose]))
+			.join(', ');
+		const optOutText = optOut ? (
+			<span
+				className="cm-opt-out"
+				title={t(['app', 'optOut', 'description'])}
+			>
+				{t(['app', 'optOut', 'title'])}
+			</span>
+		) : (
+			''
+		);
+		const requiredText = required ? (
+			<span
+				className="cm-required"
+				title={t(['app', 'required', 'description'])}
+			>
+				{t(['app', 'required', 'title'])}
+			</span>
+		) : (
+			''
+		);
 
-    render(){
-        const {checked, onToggle, name, title, description, t} = this.props
-        const required = this.props.required || false
-        const optOut = this.props.optOut || false
-        const purposes = this.props.purposes || []
-        const onChange = (e) => {
-            onToggle(e.target.checked)
-        }
-        const id = `app-item-${name}`
-        const purposesText = purposes.map((purpose) => t(['purposes', purpose])).join(", ")
-        const optOutText = optOut ? <span class="cm-opt-out" title={t(['app', 'optOut', 'description'])}>{t(['app', 'optOut', 'title'])}</span> : ''
-        const requiredText = required ? <span class="cm-required" title={t(['app', 'required', 'description'])}>{t(['app', 'required', 'title'])}</span> : ''
+		let purposesContent;
+		if (purposes.length > 0)
+			purposesContent = (
+				<p className="purposes">
+					{t(['app', purposes.length > 1 ? 'purposes' : 'purpose'])}:{' '}
+					{purposesText}
+				</p>
+			);
 
-        let purposesContent
-        if (purposes.length > 0)
-            purposesContent = <p className="purposes">{t(['app', purposes.length > 1 ? 'purposes' : 'purpose'])}: {purposesText}</p>
-
-        return <div>
-            <input id={id} class="cm-app-input" aria-describedby={`${id}-description`} disabled={required} checked={checked || required} type="checkbox" onChange={onChange} />
-            <label for={id} class="cm-app-label" {...(required ? {tabIndex: "0"} : {})}>
-                <span className="cm-app-title">{title}</span>{requiredText}{optOutText}
-                <span className={"switch"+(required ? " disabled" : "")}>
-                    <div className="slider round active"></div>
-                </span>
-            </label>
-            <div id={`${id}-description`}>
-                <p className="cm-app-description">{description || t([name, 'description'])}</p>
-                {purposesContent}
-            </div>
-        </div>
-    }
-
-}				{this.props.text == true && (
+		return (
+			<div>
+				<input
+					id={id}
+					className="cm-app-input"
+					aria-describedby={`${id}-description`}
+					disabled={required}
+					checked={checked || required}
+					type="checkbox"
+					onChange={onChange}
+				/>
+				<label
+					htmlFor={id}
+					className="cm-app-label"
+					{...(required ? { tabIndex: '0' } : {})}
+				>
+					<span className="cm-app-title">{title}</span>
+					{requiredText}
+					{optOutText}
+					<span className={'switch' + (required ? ' disabled' : '')}>
+						<div className="slider round active" />
+					</span>
+				</label>
+				{this.props.text == true && (
 					<div id={`${id}-description`}>
 						<p className="cm-app-description">
 							{description || t([name, 'description'])}
@@ -42,3 +74,7 @@ export default class AppItem extends React.Component {
 						{purposesContent}
 					</div>
 				)}
+			</div>
+		);
+	}
+}
--- src/components/apps.js
+++ src/components/apps.js
@@ -1,87 +1,57 @@
-import React from 'react'
-import AppItem from './app-item'
-import {getPurposes} from 'utils/config'
-
-class AppGroup extends React.Component {
-    render(){
-
-    }
-}
+import React from 'react';
+import AppItem from './app-item';
+import { getPurposes } from 'utils/config';
 
 export default class Apps extends React.Component {
+	constructor(props, context) {
+		super(props, context);
+		props.manager.watch(this);
+		this.state = {
+			consents: props.manager.consents
+		};
+	}
 
-    constructor(props, context){
-        super(props, context)
-        props.manager.watch(this)
-        this.state = {
-            consents : props.manager.consents
-        }
-    }
+	componentWillUnmount() {
+		const { manager } = this.props;
+		manager.unwatch(this);
+	}
 
-    componentWillUnmount(){
-        const {manager} = this.props
-        manager.unwatch(this)
-    }
+	update(obj, type, data) {
+		const { manager } = this.props;
+		if (obj == manager && type == 'consents')
+			this.setState({ consents: data });
+	}
 
-    update(obj, type, data){
-        const {manager} = this.props
-        if (obj == manager && type == 'consents')
-            this.setState({consents : data})
-    }
+	render() {
+		const { config, t, manager } = this.props;
+		const { consents } = this.state;
+		const { apps } = config;
 
-    render(){
-        const {config, t, manager} = this.props
-        const {consents} = this.state
-        const {apps} = config
+		const toggle = (apps, value) => {
+			apps.map(app => {
+				manager.updateConsent(app.name, value);
+			});
+			if (manager.confirmed) manager.saveAndApplyConsents();
+		};
 
-        const toggle = (apps, value) => {
-            apps.map((app)=>{
-                manager.updateConsent(app.name, value)
-            })
-            if (manager.confirmed)
-                manager.saveAndApplyConsents()
-        }
-
-        const toggleAll = (value) => {
-            toggle(apps, value)
-        }
-
-        const appItems = apps.map((app, key) => {
-            const toggleApp = (value) => {
-                toggle([app], value)
-            }
-            const checked = consents[app.name]
-            return <li className="cm-app">
-                <AppItem
-                    checked={checked || app.required}
-                    onToggle={toggleApp}
-                    t={t}
-                    {...app}
-                />
-            </li>
-        })
-        const allDisabled = apps.filter((app) => {
-            const required = app.required || false
-            if (required)
-                return false
-            return consents[app.name]
-        }).length == 0 ? true : false
-
-        const disableAllItem = <li className="cm-app cm-toggle-all">
-            <AppItem
-                name="disableAll"
-                title={t(['app','disableAll','title'])}
-                description={t(['app', 'disableAll', 'description'])}
-                checked={!allDisabled}
-                onToggle={toggleAll}
-                t={t}
-            />
-        </li>
-        return <ul className="cm-apps">
-            {appItems}
-            {disableAllItem}
-        </ul>
-
-    }
+		const appItems = apps.map((app, key) => {
+			const toggleApp = value => {
+				toggle([app], value);
+			};
+			const checked = consents[app.name];
+			return (
+				<li className="cm-app" key={key}>
+					<AppItem
+						checked={checked || app.required}
 						text={this.props.text}
+						onToggle={toggleApp}
+						t={t}
+						{...app}
+					/>
+				</li>
+			);
+		});
+
+		return <ul className="cm-apps">{appItems}</ul>;
+	}
 }
